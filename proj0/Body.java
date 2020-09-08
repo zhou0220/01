4247f90404daff5558e407eabeda468021422da9
zhou0220 <zzj.zhou0220@gmail.com>
2020-09-08 14:40:03 +0900

    complete part of proj0

--- proj0/Body.java
+++ proj0/Body.java
@@ -5,6 +5,7 @@ public class Body {
     public double yyVel;
     public double mass;
     public String imgFileName;
+    public static final double G = 6.67e-11;
 
     public Body(double xP, double yP, double xV, double yV, double m, String name){
         this.xxPos = xP;
@@ -22,5 +23,39 @@ public class Body {
         this.yyVel = b.yyVel;
         this.mass = b.mass;
         this.imgFileName = b.imgFileName;
+    }
+
+    public double calcDistance(Body b) {
+        double dx = b.xxPos - this.xxPos;
+        double dy = b.yyPos - this.yyPos;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double calcForceExertedBy(Body b) {
+        double r = calcDistance(b);
+        return G * mass * b.mass / (r * r);
+    }
+
+    public double calcForceExertedByX(Body b) {
+        double F = calcForceExertedBy(b);
+        double dx = b.xxPos - this.xxPos;
+        double r = calcDistance(b);
+        return F * dx / r;
+    }
+
+    public double calcForceExertedByY(Body b) {
+        double F = calcForceExertedBy(b);
+        double dy = b.yyPos - this.yyPos;
+        double r = calcDistance(b);
+        return F * dy / r;
+    }
+
+    public void update(double dt, double x_force, double y_force){
+        double ax = x_force / mass;
+        double ay = y_force / mass;
+        xxVel = xxVel + ax * dt;
+        yyVel = yyVel + ay * dt;
+        xxPos = xxPos + xxVel * dt;
+        yyPos = yyPos + yyVel * dt;
     }
 }
