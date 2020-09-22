4a4d63992a4e80a62d4aa71b5cd856338d871449
zhou0220 <zzj.zhou0220@gmail.com>
2020-09-22 12:57:51 +0900

    complete task2 of hw1

--- hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -3,12 +3,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /** Tests the ArrayRingBuffer class.
- *  @author Josh Hug
+ *  @author zhou
  */
 
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
         //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Double> x = new ArrayRingBuffer<>(4);
+        assertTrue(x.isEmpty());
+        x.enqueue(33.1);
+        x.enqueue(44.8);
+        x.enqueue(62.3);
+        x.enqueue(-3.4);
+        assertTrue(x.isFull());
+        assertEquals(33.1, x.dequeue(), 0.01);
+        assertEquals(44.8, x.peek(), 0.01);
     }
 }
