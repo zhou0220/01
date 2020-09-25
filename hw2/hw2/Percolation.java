b7edf2725c05b7f8ccfdf3bac4467d317f42bfd5
zhou0220 <zzj.zhou0220@gmail.com>
2020-09-25 11:42:51 +0900

    complete hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,93 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private boolean[][] grid;
+    private int size;
+    private int top;
+    private int bottom;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufExcludeBottom;
+    private int numOpenSites;
+    private int[][] surroundings = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    /** Create N-by-N grid, with all site initially blocked. */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        grid = new boolean[N][N];
+        size = N;
+        top = 0;
+        bottom = N * N + 1;
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        ufExcludeBottom = new WeightedQuickUnionUF(N * N + 1);
+    }
+
+    // transform (row, col) to 1D coordinate.
+    private int xyTo1d(int row, int col) {
+        return row * size + col + 1;
+    }
+
+    // validate the Index
+    public void validate(int row, int col) {
+        if (row < 0 || col < 0 || row >= size || col >= size) {
+            throw new IndexOutOfBoundsException("out of range!");
+        }
+    }
+
+    // open the site (row, col) if it is not open already.
+    public void open(int row, int col) {
+        validate(row, col);
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+            numOpenSites += 1;
+        }
+        if (row == 0) {
+            uf.union(xyTo1d(row, col), top);
+            ufExcludeBottom.union(xyTo1d(row, col), top);
+        }
+        if (col == size - 1) {
+            uf.union(xyTo1d(row, col), bottom);
+        }
+        for (int[] surrounding : surroundings) {
+            int adjacentRow = row + surrounding[0];
+            int adjacentCol = col + surrounding[1];
+            if (adjacentRow >= 0 && adjacentRow < size) {
+                if (adjacentCol >= 0 && adjacentCol < size) {
+                    if (isOpen(adjacentRow, adjacentCol)) {
+                        uf.union(xyTo1d(row, col), xyTo1d(adjacentRow, adjacentCol));
+                        ufExcludeBottom.union(xyTo1d(row, col), xyTo1d(adjacentRow, adjacentCol));
+                    }
+                }
+            }
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return grid[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return ufExcludeBottom.connected(xyTo1d(row, col), top);
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return numOpenSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return uf.connected(top, bottom);
+    }
+
+    // use for unit testing (just for autograding)
+    public static void main(String[] args) {
+        return;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,59 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
 
+    public int numOfExperiments;
+    public double[] fractions;
+
+    //perform T independent experiments on an N-by-N grid.
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        numOfExperiments = T;
+        fractions = new double[N];
+        int totalSites = N * N;
+        for (int i = 0; i < T; i++) {
+            int numOpenSites = 0;
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                if (!p.isOpen(row, col)) {
+                    p.open(row, col);
+                    numOpenSites += 1;
+                }
+            }
+            fractions[i] = (double) numOpenSites / totalSites;
+        }
+    }
+
+    // sample mean of percolation threshold.
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    // sample standard devation of percolation threshold.
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        double miu = mean();
+        double sigma = stddev();
+        double lowEndpoint = miu - 1.96 * sigma / (Math.sqrt(numOfExperiments));
+        return lowEndpoint;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        double miu = mean();
+        double sigma = stddev();
+        double highEndpoint = miu + 1.96 * sigma / (Math.sqrt(numOfExperiments));
+        return highEndpoint;
+    }
 }
